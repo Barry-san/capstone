2467c5694ad5acd84ae72135d64155413b7c3232
Barry-san <oyeyemimubarak6@gmail.com>
2025-05-18 21:08:42 +0100

    moved app features into pages structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
 import { Footer } from "./common/Footer";
 import { Navbar } from "./common/Navbar";
-import { Hero } from "./landing/components/Hero";
+import { AppRoutes } from "./routes";
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <Hero />
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
       <Footer />
     </div>
   );
